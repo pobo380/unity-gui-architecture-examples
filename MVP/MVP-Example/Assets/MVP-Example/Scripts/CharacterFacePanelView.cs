19a091a58cdfd817a0db886caa6293aa4bad0ecb
pobo380 <kohei.noda@mixi.co.jp>
2017-12-19 20:02:06 +0900

    wip2

---
+++ MVP/MVP-Example/Assets/MVP-Example/Scripts/CharacterFacePanelView.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[DisallowMultipleComponent]
+public class CharacterFacePanelView : MonoBehaviour
+{
+    // properties
+
+    [SerializeField]
+    private Image _characterFace;
+
+    [SerializeField]
+    private Image _elementFrame;
+
+    [SerializeField]
+    private Button _button;
+
+    public Action OnClick = delegate { };
+
+    // public methods
+
+    public void Setup()
+    {
+        _button.onClick.AddListener(ButtonOnClick);
+    }
+
+    public void SetCharacterFace(Sprite sprite)
+    {
+        _characterFace.sprite = sprite;
+    }
+
+    public void SetElementFrame(Sprite sprite)
+    {
+        _elementFrame.sprite = sprite;
+    }
+
+    // private methods
+
+    private void ButtonOnClick()
+    {
+        if (OnClick != null)
+        {
+            OnClick();
+        }
+    }
+}
