855b9bf0d119b2bba8ba82e885bed39e2d83cce5
pobo380 <kohei.noda@mixi.co.jp>
2017-12-26 20:47:35 +0900

    リファクタwip

---
+++ MVP/MVP-Example/Assets/MVP-Example/Scripts/Model/CharacterModel.cs
@@ -0,0 +1,15 @@
+﻿using UniRx;
+
+public class CharacterModel
+{
+    // properties
+
+    private ReactiveProperty<bool> _favorite;
+
+    public IReadOnlyReactiveProperty<bool> Favorite
+    {
+        get { return _favorite; }
+    }
+
+    // public method
+}
